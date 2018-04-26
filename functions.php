ba06598ca34eff7cc889dcc4be623ae8dcb68a64
Logan Call <logancall.dev@gmail.com>
2018-04-26 16:56:20 -0600

    Remove acf-field-separator, as Groups are native in acf 5.6+

--- functions.php
+++ functions.php
@@ -5,5 +5,4 @@ require_once('settings/options-pages.php');
 require_once('settings/reboot.php');
 require_once('settings/support.php');
 require_once('utils/post-duplicator.php');
-require_once('utils/acf-field-separator.php');
 require_once('utils/post-order/post-order.php');
